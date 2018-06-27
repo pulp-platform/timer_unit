673f7f26e0da0cec7cf51db7d2e923003087b4f9
Florent Rotenberg <florent.rotenberg@greenwaves-technologies.com>
2018-06-27 14:41:15 +0200

    Fix multiple bugs (Tuleap #253, #254): 64b mode did not worked at all, prescaler + refclk failed, oneshot in 64b mode failed, cmpclr in 64b mode failed.

--- rtl/timer_unit_counter_presc.sv
+++ rtl/timer_unit_counter_presc.sv
@@ -1,10 +1,10 @@
 // Copyright 2014-2018 ETH Zurich and University of Bologna.
 // Copyright and related rights are licensed under the Solderpad Hardware
-// License, Version 0.51 (the "License"); you may not use this file except in
+// License, Version 0.51 (the “License”); you may not use this file except in
 // compliance with the License. You may obtain a copy of the License at
 // http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
 // or agreed to in writing, software, hardware and materials distributed under
-// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// this License is distributed on an “AS IS” BASIS, WITHOUT WARRANTIES OR
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
@@ -31,21 +31,21 @@ module timer_unit_counter_presc
    // COUNTER
    always_comb
    begin
-      s_count = s_count_reg;
+     s_count = s_count_reg;
       
-      // start counting
-      if ( reset_count_i == 1 || target_reached_o==1  || enable_count_i==0)
-         s_count = 0;
-      else
-	begin
-	   if (write_counter_i == 1) // OVERWRITE COUNTER
-	     s_count = counter_value_i;
-	   else
-	     begin
-		if ( enable_count_i == 1 ) // the counter is increased if counter is enabled and there is a tick
-		  s_count = s_count_reg + 1;
-	     end
-	end
+     // start counting
+     if ( reset_count_i == 1 || target_reached_o==1)
+       s_count = 0;
+     else
+     begin
+        if (write_counter_i == 1) // OVERWRITE COUNTER
+           s_count = counter_value_i;
+        else
+        begin
+             if ( enable_count_i == 1 )
+               s_count = s_count_reg + 1;
+        end
+     end
    end
    
    always_ff@(posedge clk_i, negedge rst_ni)
